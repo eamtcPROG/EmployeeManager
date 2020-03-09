21c408890a0385a02dd555c5e16abb26de897a4c
eamtc <prog.eamtc@gmail.com>
2020-03-09 20:50:51 +0200

    Second commit

--- src/com/company/EmployeeManager.java
+++ src/com/company/EmployeeManager.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeManager {
-    List employeeList = new ArrayList();
-    public void Add(int id,String nume,String prenume,String idnp, String gen ){
+
+    public void Add(List employeeList,int id,String nume,String prenume,String idnp, String gen ){
         Employee em = new Employee();
         em.setId(id);
         em.setNume(nume);
@@ -14,5 +14,28 @@ public class EmployeeManager {
         em.setGen(gen);
         employeeList.add(id,em);
     }
-
+    public void Edit(List employeeList,Employee em,int id,String nume,String prenume,String idnp, String gen ){
+        em.setNume(nume);
+        em.setPrenume(prenume);
+        em.setIdnp(idnp);
+        em.setGen(gen);
+        employeeList.set(id,em);
+    }
+    public void Delete(List employeeList,int id){
+        employeeList.remove(id);
+    }
+    public void View(List employeeList){
+        System.out.print("| Id |      Nume      |      Prenume      |      Idnp      |      Gen      |");
+        System.out.print("----------------------------------------------------------------------------");
+        for (int i = 0;i< employeeList.size();i++){
+           Employee em = (Employee) employeeList.get(i);
+           String nume = em.getNume();
+           String prenume = em.getPrenume();
+           String idnp = em.getIdnp();
+           String gen = em.getGen();
+           int id = em.getId();
+           System.out.println("| " + id + " | " + nume + " | "+ prenume+" | "+" | "+ idnp + " | "+ gen + " |");
+            System.out.print("----------------------------------------------------------------------------");
+       }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,67 @@
 package com.company;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+	public static void clearScreen() {
+		//
+	}
+	public static void ShowMeniu(){
+		System.out.println("\t\t\t-----------------------------Meniu-----------------------------");
+		System.out.println("\t\t\t--------------------- 1 - Add     -----------------------------");
+		System.out.println("\t\t\t--------------------- 2 - Edit    -----------------------------");
+		System.out.println("\t\t\t--------------------- 3 - Delete  -----------------------------");
+		System.out.println("\t\t\t--------------------- 4 - View    -----------------------------");
+		System.out.println("\t\t\t--------------------- 5 - Exit    -----------------------------");
+	}
 
     public static void main(String[] args) {
-	    Employee em = new Employee();
-	    em.setNume("Dan");
-	    String nume = em.getNume();
-	    System.out.print(nume);
+	    Employee employee = new Employee();
+		EmployeeManager employeeManager = new EmployeeManager();
+		List employeeList = new ArrayList();
+		Scanner scan = new Scanner(System.in);
+		byte finish = 0;
+		char input;
+		do {
+			clearScreen();
+			ShowMeniu();
+			input = scan.next().charAt(0);
+			switch (input){
+				case '1':{
+					clearScreen();
+					System.out.print("Add");
+					break;
+				}
+				case '2':{
+					clearScreen();
+					System.out.print("Edit");
+					break;
+				}
+				case '3':{
+					clearScreen();
+					System.out.print("Delete");
+					break;
+				}
+				case '4':{
+					clearScreen();
+					System.out.print("View");
+					break;
+				}
+				case '5':{
+					clearScreen();
+					System.out.print("Exit");
+					finish = 1;
+					break;
+				}
+				default:{
+					clearScreen();
+					System.out.print("Enter 1|2|3|4|5 only");
+				}
+			}
+
+		}while (finish != 1);
     }
 }
